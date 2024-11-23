e787d4f6bbb4cee8081aaa8b739de55a394c44bc
Bat_Gosho <goshkodark25@gmail.com>
2024-11-23 13:28:00 +0200

    Add files via upload

---
+++ zad13.java
@@ -0,0 +1,5 @@
+public class zad13 {
+    public static void main(String[] args) {
+        //Formatting numbers 
+    }
+}
---
+++ zad14.java
@@ -0,0 +1,15 @@
+public class zad14 {
+    public static void main(String[] args) {
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;//prevrusjta stringa v int(moje i float,short ... zavisi in na4alniq tip
+        //int => na4alen tip
+        //polzva se v gameDev. kato user input-ta sa stringove
+        System.out.println(y);
+    }
+}
+/*
+double x = 1.1;
+int y = x + 1;
+
+trqbva tiput da e edin i susht( int y ==> double y)
+ */
